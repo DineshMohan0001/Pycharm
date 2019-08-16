937bfffb098e262c80cb65d5ceb9c262e343af52
DineshMohan0001 <53462648+DineshMohan0001@users.noreply.github.com>
2019-08-16 15:16:39 +0530

    local update

--- Sample.py
+++ Sample.py
@@ -2,3 +2,5 @@ print("HEllo")
 print("this is at the windows")
 #update successful
 #let it be in the branch
+#%%%%%we canot find the updates inn the master
+#these are written in the pycharm for the branch%%%%%
